f17ce8f81a73e3cd8e1f5bfa0e580844c45b443c
Garrett <garrett.d.udstrand@gmail.com>
2022-06-24 16:19:47 -0500

    CRUD operations accessible via browser

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod pool;
 
 use migration::{MigratorTrait, tests_cfg::json};
 use pool::Db;
-use rocket::{fairing::{AdHoc, self}, Rocket, Build, form::Form, serde::json::Json, http::Status, response::{Responder, self}, Request};
+use rocket::{fairing::{AdHoc, self}, Rocket, Build, form::Form, serde::json::Json, http::Status, response::{Responder, self, Flash, Redirect}, Request, fs::{FileServer, relative}, request::FlashMessage};
 use rocket_dyn_templates::Template;
 use sea_orm::{ActiveModelTrait, Set, EntityTrait, QueryOrder, DeleteResult};
 use sea_orm_rocket::{Database, Connection};
@@ -28,7 +28,7 @@ impl From<sea_orm::DbErr> for DatabaseError {
 }
 
 #[post("/addtask", data="<task_form>")]
-async fn add_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Result<Json<tasks::Model>, DatabaseError> {
+async fn add_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Result<Flash<Redirect>, DatabaseError> {
     let db = conn.into_inner();
     let task = task_form.into_inner();
 
@@ -37,7 +37,9 @@ async fn add_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Re
         ..Default::default()
     };
 
-    Ok(Json(active_task.insert(db).await?))
+    active_task.insert(db).await?;
+
+    Ok(Flash::success(Redirect::to("/"), "Task created!"))
 }
 
 #[get("/readtasks")]
@@ -53,29 +55,28 @@ async fn read_tasks(conn: Connection<'_, Db>) -> Result<Json<Vec<tasks::Model>>,
 }
 
 #[put("/edittask", data="<task_form>")]
-async fn edit_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Result<Json<tasks::Model>, DatabaseError> {
+async fn edit_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Result<Flash<Redirect>, DatabaseError> {
     let db = conn.into_inner();
     let task = task_form.into_inner();
 
     let task_to_update = Tasks::find_by_id(task.id).one(db).await?;
     let mut task_to_update: tasks::ActiveModel = task_to_update.unwrap().into();
     task_to_update.item = Set(task.item);
+    task_to_update.update(db).await?;
 
-    Ok(Json(
-        task_to_update.update(db).await?
-    ))
+    Ok(Flash::success(Redirect::to("/"), "Task edited succesfully!"))
 }
 
 #[delete("/deletetask/<id>")]
-async fn delete_task(conn: Connection<'_, Db>, id: i32) -> Result<String, DatabaseError> {
+async fn delete_task(conn: Connection<'_, Db>, id: i32) -> Result<Flash<Redirect>, DatabaseError> {
     let db = conn.into_inner();
-    let result = Tasks::delete_by_id(id).exec(db).await?;
+    let _result = Tasks::delete_by_id(id).exec(db).await?;
 
-    Ok(format!("{} task(s) deleted", result.rows_affected))
+    Ok(Flash::success(Redirect::to("/"), "Task succesfully deleted!"))
 }
 
 #[get("/")]
-async fn index(conn: Connection<'_, Db>) -> Result<Template, DatabaseError> {
+async fn index(conn: Connection<'_, Db>, flash: Option<FlashMessage<'_>>) -> Result<Template, DatabaseError> {
     let db = conn.into_inner();
     let tasks = Tasks::find()
                             .order_by_asc(tasks::Column::Id)
@@ -85,7 +86,21 @@ async fn index(conn: Connection<'_, Db>) -> Result<Template, DatabaseError> {
     Ok(Template::render(
         "todo_list",
         json!({
-            "tasks": tasks
+            "tasks": tasks,
+            "flash": flash.map(FlashMessage::into_inner)
+        })
+    ))
+}
+
+#[get("/edit/<id>")]
+async fn edit_task_page(conn: Connection<'_, Db>, id: i32) -> Result<Template, DatabaseError> {
+    let db = conn.into_inner();
+    let task = Tasks::find_by_id(id).one(db).await?.unwrap();
+
+    Ok(Template::render(
+        "edit_task_form", 
+        json!({
+            "task": task
         })
     ))
 }
@@ -101,6 +116,7 @@ fn rocket() -> _ {
     rocket::build()
         .attach(Db::init())
         .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
-        .mount("/", routes![index, add_task, read_tasks, edit_task, delete_task])
+        .mount("/", FileServer::from(relative!("/public")))
+        .mount("/", routes![index, edit_task_page, add_task, read_tasks, edit_task, delete_task])
         .attach(Template::fairing())
 }
